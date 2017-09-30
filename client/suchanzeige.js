3eb48c97c179670def32e0b1dbca4289909a3142
= <what.thefrog@web.de>
2017-09-30 22:12:32 +0200

    Aufraeumen

--- client/suchanzeige.js
+++ client/suchanzeige.js
@@ -1,17 +1,21 @@
 function suchAnzeige(jsonData){
     window.location = "suchergebnisse.html";
     var htmlStr = "";
-    for(item in jsonData){
+    items = jsonData.items;
+    console.log(items);
+    for(var i in items){
+        console.log(items[i]);
         htmlStr+= "<h1>Ergebnisse</h1>" + 
-        "<h3><a href=" + ">" + item.title + "</a></h3>" + 
-        "<p>" + item.class + "</p>" +
-        "<p>" + item.userName + "</p>" +
-        "<p>" + item.keywords + "</p>" +
-        "<p>" + item.date + "</p>" +
-        "<p>" + item.text + "</p><hr>"; 
+        "<h3><a href=" + ">" + items[i].title + "</a></h3>" + 
+        "<p>" + items[i].class + "</p>" +
+        "<p>" + items[i].userName + "</p>" +
+        "<p>" + items[i].keywords + "</p>" +
+        "<p>" + items[i].date + "</p>" +
+        "<p>" + items[i].text + "</p><hr>"; 
     }
     
-    window.onload(function(){
+    console.log("html", htmlStr);
+    window.onload = function(){
         document.getElementById("ergebnisse").innerHTML = htmlStr;
-    });
+    };
 }
