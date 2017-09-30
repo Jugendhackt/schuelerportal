7999f5ba850ae6e145a67d2d9f6455fa76c20aa6
Anton Kraft <github@akraft.org>
2017-09-30 18:12:05 +0200

    Menu Update

--- client/faecher.js
+++ client/faecher.js
@@ -1,7 +1,7 @@
 function faecherLoad(){
     var url = "http://192.168.9.129:3000/subjects";
     var faecherliste;
-    
+
     //Faecherliste anfordern
     fetch(url, {
         method: 'get'
@@ -14,8 +14,14 @@ function faecherLoad(){
     }).catch(function(err) {
 	   console.log("there was an error" + err);
     });
-    
+
 }
 
 function faecherAnzeigen(liste){
+  for (var i = 0; i < liste.subjects.length; i++) {
+    console.log(liste.subjects[i])
+    var fach = liste.subjects[i].subjectName;
+    document.getElementById('faecherliste').appendChild(document.createElement('li')).appendChild(document.createTextNode(fach))
+  }
+
 }
