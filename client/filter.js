e29a7512c52e76d6b74bea65e4e5a74ff4f52c9d
= <what.thefrog@web.de>
2017-09-30 17:31:50 +0200

    Upload Formular erstellt, Suchfunktion erweitert

--- client/filter.js
+++ client/filter.js
@@ -1,15 +1,39 @@
 function suchRequestSimple(){
-    var suchRequest;
+    var url = "http://192.168.9.129:3000/search";
+    var suchRequest = {};
     
-    suchRequest.volltext = document.forms["suchFeldOben"]["suchText"];
+    suchRequest.keywords = document.forms["suchFeldOben"]["suchText"].value.split(',');
     userInfo = JSON.parse(localStorage.getItem('userInfo'));
-    if(user != "undefined"){
+    if(userInfo != "undefined"){
         suchRequest.class = userInfo.class;
     }
     
+    //Daten senden
+    fetch(url, {
+        method: 'post',
+        body: JSON.stringify(suchRequest),
+        headers: new Headers({
+		'Content-Type': 'application/json'
+	   })
+    }).then(function(response) {
+        return response.json();
+    }).then(function(json){
+        console.log("json: ", json);
+        suchResponse(json);
+    }).catch(function(err) {
+	   console.log("there was an error" + err);
+    });
+    
     localStorage.setItem("lastSearch", JSON.stringify(suchRequest));
+    return false;
 }
 
+function suchRequest(){
+    
+}
+function suchResponse(json){
+    
+}
 function fillOutFilter(){
     userInfo = JSON.parse(localStorage.getItem('userInfo'));
     lastSuchRequest = localStorage.getItem('last')
