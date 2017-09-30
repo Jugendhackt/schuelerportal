25eca87442eab76f7d3a268362af3ff7dddddf58
= <what.thefrog@web.de>
2017-09-30 23:20:33 +0200

    suchanzeige

--- client/suchanzeige.js
+++ client/suchanzeige.js
@@ -5,25 +5,19 @@ function suchAnzeige(){
     console.log(items);
     for(var i in items){
         console.log(items[i]);
+        var text = items[i].text;
+        if(text > 25){
+            text = text.substring(0,24) + "...";
+        }
         htmlStr+= "<h1>Ergebnisse</h1>" + 
         "<h3><a href=" + ">" + items[i].title + "</a></h3>" + 
         "<p>Klassenstufe: " + items[i].class + "</p>" +
         "<p>User: " + items[i].userName + "</p>" +
         "<p>Keywords: " + items[i].keywords + "</p>" +
         "<p>Datum: " + items[i].date + "</p>" +
-        "<p>Text: " + items[i].text + "</p><hr>"; 
+        "<p>Text: " + text + "</p><hr>"; 
     }
     
     console.log("html", htmlStr);
     document.getElementById("ergebnisse").innerHTML = htmlStr;
-    /*
-    window.addEventListener("load", function(){
-        console.log("window loaded");
-        document.getElementById("ergebnisse").innerHTML = htmlStr;
-    });
-    
-    window.onload = function(){
-        console.log("window loaded");
-        document.getElementById("ergebnisse").innerHTML = htmlStr;
-    };*/
 }
