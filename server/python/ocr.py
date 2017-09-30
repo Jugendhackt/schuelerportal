b99e73cd936e17dda6ea828acd3f611666034d1c
Anton Kraft <github@akraft.org>
2017-09-30 11:23:39 +0200

    Ocr hinzugefügt

---
+++ server/python/ocr.py
@@ -0,0 +1,12 @@
+import sys
+bild = sys.argv[1:]
+bild = ''.join(bild)
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+
+print pytesseract.image_to_string(Image.open(bild))
