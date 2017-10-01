ef3d4d8ab48b08281ff740fb63f9ed3ac5f9eb4f
= <what.thefrog@web.de>
2017-10-01 10:13:46 +0200

    Anzeige von Dokumenten funktioniert nun

--- client/documentanzeige.js
+++ client/documentanzeige.js
@@ -2,8 +2,15 @@ function documentAnzeige(){
     var documentJson = JSON.parse(localStorage.getItem("documentAnz"));
     console.log("document", documentJson);
     document.getElementById('title').innerHTML = documentJson.title;
+    document.getElementById('docmetadata').innerHTML = jsonToHTML(documentJson);
     document.getElementById('text').innerHTML = documentJson.text;
-    document.getElementById('image').innerHTML = `<img src="http://192.168.9.129:3000/uploadedData/` + documentJson.tmpHash + "." +  documentJson.fileExtension + '">';
-    
-    
+    document.getElementById('image').innerHTML = `<img style="max-width: 100%; max-height: 100%" src="http://192.168.9.129:3000/` + documentJson.tmpHash + "." +  documentJson.fileExtension + '">';
+}
+
+function jsonToHTML(item){ 
+    var htmlStr = "<p>Klassenstufe: " + item.class + "</p>" +
+        "<p>User: " + item.userName + "</p>" +
+        "<p>Keywords: " + item.keywords + "</p>" +
+        "<p>Datum: " + item.date + "</p>";
+    return htmlStr;
 }
